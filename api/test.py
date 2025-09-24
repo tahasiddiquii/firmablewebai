79035f00705fe27398dda89544e1e4c13af12d47
tahasiddiquii <studies.tahasiddiqui@gmail.com>
2025-09-24 08:07:55 +0530

    Add simple test endpoint and copy frontend to public

---
+++ api/test.py
@@ -0,0 +1,24 @@
+from http.server import BaseHTTPRequestHandler
+import json
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        
+        response = {
+            "status": "success",
+            "message": "FirmableWebAI API is working!",
+            "service": "test-endpoint"
+        }
+        
+        self.wfile.write(json.dumps(response).encode())
+    
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        self.end_headers()
